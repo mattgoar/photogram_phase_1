dce08cdb635ecccb61e5e4614ad6b309bb3a051a
mattgoar <mattgoar@gmail.com>
2015-07-28 12:16:30 -0500

    Completed assignment

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,20 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    photo_in = params[:id]
+    @photo = Photo.where(:id => photo_in)
+  end
+
+  def destroy
+    photo_in = params[:id]
+    Photo.destroy(photo_in)
+    @list_of_photos = Photo.all
+    render("index.html.erb")
+  end
+
+  def new_form
+        render("new_form.html.erb")
+  end
 
 end
